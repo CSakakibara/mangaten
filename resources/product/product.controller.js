2ef779a71c343a9d56d5197b0355c28a7bf39356
Christopher Jun Maruyama Sakakibara <cj-sakakib@hotmail.com>
2019-11-27 22:16:18 -0300

    controller

--- resources/product/product.controller.js
+++ resources/product/product.controller.js
@@ -15,18 +15,50 @@ async function getOne(req, res) {
   }
 }
 
-async function getMany() {
+async function getMany(req, res) {
+  const productCollection = client.db('mangaten').collection('product') //define a coleção
 
+  const mangas = await productCollection.find({}).toArray() //traz todos os itens da coleção
+
+  res.status(200).json({ product: mangas }) //retorna os itens
 }
 
-async function createOne() {
 
+async function createOne(req, res) { //cadastro manga
+  // const user = req.user //pegar dados do usuario verificado no protect
+
+  const manga = req.body //pega os dados na requisição
+  // manga.createdBy = user._id // associa no item a ser inserido no banco o id do usuario que esta fazendo o cadastro
+
+  const productCollection = client.db('mangaten').collection('product') //define a coleção do banco
+  await productCollection.insertOne(manga) //insere os dados na coleção
+  res.status(201).json({ message: 'Manga Cadastrado' })
 }
 
-async function removeOne() {
+async function removeOne(req, res) {
+    const id = req.params.id// recebe o id
+    // const user = req.user// recebe os dados do usuario
 
+    const productCollection = client.db('mangaten').collection('product')//referencia a coleção de produtos
+
+    try{
+      const deletedProduct = await productCollection.findOneAndDelete({ // fazer alterações para verificar se o usuario é adm
+        _id: ObjectId(id)
+        // createdBy: ObjectId(user._id)
+      })
+      console.log('deletedProduct', deletedProduct.value)
+
+      const manga = deletedProduct.value
+
+      res.status(200).json(manga) //retorna mensagem
+    } catch(error){
+      res.status(404).end()
+    }
 }
 
 module.exports = {
-  getOne
+  getOne,
+  getMany,
+  createOne,
+  removeOne
 }
--- resources/product/product.router.js
+++ resources/product/product.router.js
@@ -13,41 +13,12 @@ const controller = require('./product.controller')
 
 // /product
 router.route('/')
-  .get(async (req, res) => {
-    const productCollection = client.db('mangaten').collection('product') //define a coleção
-
-    const mangas = await productCollection.find({}).toArray() //traz todos os itens da coleção
-
-    res.status(200).json({ product: mangas }) //retorna os itens
-  })
-  .post(async (req, res) => { //cadastro manga
-    // const user = req.user //pegar dados do usuario verificado no protect
-
-    const manga = req.body //pega os dados na requisição
-    // manga.createdBy = user._id // associa no item a ser inserido no banco o id do usuario que esta fazendo o cadastro
-
-    const productCollection = client.db('mangaten').collection('product') //define a coleção do banco
-    await productCollection.insertOne(manga) //insere os dados na coleção
-    res.status(201).json({ message: 'Manga Cadastrado' })
-  })
+  .get(controller.getMany)
+  .post(controller.createOne)
 
 // /product/:id
 router.route('/:id')
   .get(controller.getOne)
-  .delete(async (req, res) => {
-    const id = req.params.id// recebe o id
-    // const user = req.user// recebe os dados do usuario
-
-    const productCollection = client.db('mangaten').collection('product')//referencia a coleção de manga
-    const deletedProduct = await productCollection.findOneAndDelete({ // fazer alterações para verificar se o usuario é adm
-      _id: ObjectId(id)
-      // createdBy: ObjectId(user._id)
-    })
-    console.log('deletedProduct', deletedProduct.value)
-
-    const manga = deletedProduct.value
-
-    res.status(200).json(manga) //retorna mensagem
-  })
+  .delete(controller.removeOne)
 
 module.exports = router
