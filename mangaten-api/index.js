dce6568a6f135edeee26b62dcf0a9f590ad809f9
Christopher Jun Maruyama Sakakibara <cj-sakakib@hotmail.com>
2019-12-14 01:19:04 -0300

    fr

--- mangaten-api/index.js
+++ mangaten-api/index.js
@@ -1,7 +1,8 @@
 // vendors
 const express = require('express') // usado pra criar servidor http
+const cors = require('cors') // usado pra criar servidor http
 
-const PORT = 3000 // porta a ser usada posteriormente com express
+const PORT = 5000 // porta a ser usada posteriormente com express
 const app = express() // cria uma aplicação express
 
 // db
@@ -16,12 +17,15 @@ const usersRouter = require('./resources/user/user.router')
 
 // middlewares
 app.use(express.json()) // traz os dados do corpo da requição pro req.body
+app.use(cors())
 
 app.post('/signup', signUp)
 app.post('/signin', signIn)
 app.use('/products', productsRouter)
 app.use('/users', protect, usersRouter)
 app.post('/admin/signup', protect, protectAdmin, signUpAdmin)
+
+
 
 async function main () { // inicia o servidor
   await connectToMongoDB()
