4f3b35550d8b0a6ac4ae09671b4b88d8a7a63d37
Christopher Jun Maruyama Sakakibara <cj-sakakib@hotmail.com>
2019-12-17 00:34:30 -0300

    stock up

--- mangaten-api/index.js
+++ mangaten-api/index.js
@@ -2,7 +2,7 @@
 const express = require('express') // usado pra criar servidor http
 const cors = require('cors') // usado pra criar servidor http
 
-const PORT = 5000 // porta a ser usada posteriormente com express
+const PORT = process.env.PORT || 5000 // porta a ser usada posteriormente com express
 const app = express() // cria uma aplicação express
 
 // db
@@ -16,11 +16,11 @@ const { shipping } = require('./utils/shipping')
 // routes
 const productsRouter = require('./resources/product/product.router')
 const usersRouter = require('./resources/user/user.router')
-
+const Product = require('./resources/product/product.model')
 // middlewares
 app.use(cors())
 app.use(express.json()) // traz os dados do corpo da requição pro req.body
-app.use('/images', express.static('uploads/images')); //disponibiliza as imagens pelo nome na rota /images/namefile
+app.use('/images', express.static('uploads/images')) // disponibiliza as imagens pelo nome na rota /images/namefile
 
 app.post('/signup', signUp)
 app.post('/signin', signIn)
@@ -28,8 +28,27 @@ app.post('/admin/signup', protect, protectAdmin, signUpAdmin)
 app.get('/shipping/:cep', shipping)
 app.use('/products', productsRouter)
 app.use('/users', protect, usersRouter)
+app.post('/batata', protect, async (req, res) => {
+  const cart = req.body.cart
 
-async function main() { // inicia o servidor
+  const products = []
+  for (let i = 0; i < cart.length; i++) {
+    const item = cart[i]
+
+    const update = {
+      $inc: {
+        stock: item.total * (-1)
+      }
+    }
+
+    const product = await Product.findByIdAndUpdate(item.product._id, update, { new: true }).lean().exec()
+    products.push(product)
+  }
+
+  res.json({ products })
+})
+
+async function main () { // inicia o servidor
   await connectToMongoDB()
 
   await app.listen(PORT)
